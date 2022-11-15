44b53abbfd3caffc8c2eb4569c509b3d59a46927
Maxxus220 <Maxxus220@gmail.com>
2022-11-14 21:50:25 -0600

    Working on refactor

--- execute.v
+++ execute.v
@@ -5,7 +5,7 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 module execute (clk, rst, Reg1, Reg2, JumpOffset, PCplus2, instr_imm, ExtMode, IType, Reg1Rev, Reg1Shift, 
-				Zero1, Zero2, ALUSrc, BrOp, Branch, Jump, CompCarry, ALUComp, ALUOp, sign, PcVal, ALUOut, DataOut, JBAdr, WriteData, BrJmpTaken);
+				Zero1, Zero2, ALUSrc, BrOp, Branch, Jump, CompCarry, ALUComp, ALUOp, sign, PCVal, ALUOut, DataOut, JBAdr, WriteData, BrJmpTaken);
 
 	// clk/rst
 	input clk, rst;
@@ -18,7 +18,7 @@ module execute (clk, rst, Reg1, Reg2, JumpOffset, PCplus2, instr_imm, ExtMode, I
 	input ExtMode, IType, Reg1Rev, Reg1Shift, Zero1, Zero2, ALUSrc, Branch, Jump, CompCarry, ALUComp;
 	input [2:0] ALUOp, BrOp;  
 	input sign;
-	input PcVal;
+	input PCVal;
 	// data outputs
 	output [15:0] ALUOut; // to memory
 	output [15:0] DataOut; // to wb
@@ -135,6 +135,6 @@ module execute (clk, rst, Reg1, Reg2, JumpOffset, PCplus2, instr_imm, ExtMode, I
 	// select between ALUOut or branch comparison (including carry bit)
 	assign RawComp = {15'h0000, BrOpOut}; // extend RawComp to 16 bits before selecting between it and ALUOut
 	mux16_2 MUX_ALUComp(.out(DataOut), .in0(ALUOut), .in1(RawComp), .sel(ALUComp));
-	assign BrJmpTaken = BrEn | (PcVal & ~Branch);
+	assign BrJmpTaken = BrEn | (PCVal & ~Branch);
    
 endmodule
--- proc.v
+++ proc.v
@@ -23,12 +23,16 @@ module proc (/*AUTOARG*/
    
    /* your code here -- should include instantiations of fetch, decode, execute, mem and wb modules */
    
-   // I/O signals
+
+   //////////////////
+   // I/O SIGNALS //
+   ////////////////
+   /* Instantiations of intermediary wires */
+
 	wire [15:0] JBAdr;
-	
 	wire [15:0] instr_FETCH, instr_DECODE, instr_EXECUTE, instr_MEMORY, instr_WB;
 	wire [15:0] instr_DECODE_temp, instr_EXECUTE_temp, instr_MEMORY_temp, instr_WB_temp;
-	wire [15:0] instr_IN; // instruction that enters the fetch block after considering stalls
+	wire [15:0] instr_IN; // Choice between normal instr and stall (nop)
 	wire [15:0] PCplus2_FETCH, PCplus2_DECODE, PCplus2_EXECUTE, PCplus2_MEMORY, PCplus2_WB;
 	wire [15:0] RegData_WB;
 	wire [15:0] Reg1_DECODE, Reg2_DECODE, Reg1_EXECUTE, Reg2_EXECUTE;
@@ -40,28 +44,33 @@ module proc (/*AUTOARG*/
 	wire [15:0] MemOut_MEMORY, MemOut_WB;
 	wire BrJmpTaken;
 
-	// Control signals
+	//////////////////////
+	// CONTROL SIGNALS //
+	////////////////////
+	/* Instatiations of control wires */
+
 	wire [4:0]  Control;
 	wire [26:0] ControlSignals_DECODE, ControlSignals_EXECUTE, ControlSignals_MEMORY, ControlSignals_WB;
-	
-	wire stall;
-	//wire stall_post;
+
+	wire Stall;
 	wire Halt;
-	
+
 	wire [15:0] fill, fillplus1, newfill;
 	wire dummy;
+
 	cla_16b FILL(.sum(fillplus1), .c_out(dummy), .a(fill), .b(16'h0001), .c_in(1'b0));
 	assign newfill = (fill == 16'h0004 ? fill : fillplus1);
 	dff WAIT [15:0] (.q(fill), .d(newfill), .clk(clk), .rst(rst));
 	
 	
 	//////////////////////////////
-	// Control Signal Pipeline //
+	// CONTROL SIGNAL PIPELINE //
 	////////////////////////////
+	/* Contains Control Block which generates control signals and latches for control signals */
+
 	// Control Block
 	control CONTROL(.clk(clk), .rst(rst), .Control(Control), .ALUControl(instr_imm_DECODE[1:0]), .ControlSignals(ControlSignals_DECODE));
-	// Fetch/Decode
-	// dff FD_FF [27:0] (.q({}), .d({}), .clk(clk), .rst(rst));
+
 	// Decode/Execute
 	dff DX_FF [26:0] (.q({ControlSignals_EXECUTE}), .d({ControlSignals_DECODE}), .clk(clk), .rst(rst | BrJmpTaken));
 	// Execute/Memory
@@ -69,114 +78,90 @@ module proc (/*AUTOARG*/
 	// Memory/Write-Back
 	dff MW_FF [26:0] (.q({ControlSignals_WB}), .d({ControlSignals_MEMORY}), .clk(clk), .rst(rst));
 	
+
 	///////////////////////////
-	// Data Signal Pipeline //
+	// DATA SIGNAL PIPELINE //
 	/////////////////////////
-	// fetch-decode flip flop	
+	/* Contains latches for data signals */
+
+	// Fetch/Decode	
 	dff FD_FF [31:0] (.q({PCplus2_DECODE,instr_DECODE_temp}), .d({PCplus2_FETCH,instr_IN}), .clk(clk), .rst(rst | BrJmpTaken));
-	// decode-execute data flip flop
+	// Decode/Execute
    	dff DX_DATA_FF [71:0] (.q({instr_imm_EXECUTE,JumpOffset_EXECUTE,PCplus2_EXECUTE,Reg2_EXECUTE,Reg1_EXECUTE}), 
    						   .d({instr_imm_DECODE,JumpOffset_DECODE,PCplus2_DECODE,Reg2_DECODE,Reg1_DECODE}), 
    						   .clk(clk), .rst(rst | BrJmpTaken));
-   	// execute-memory data flip flop
+   	// Execute/Memory
    	dff XM_DATA_FF [63:0] (.q({PCplus2_MEMORY,DataOut_MEMORY,WriteData_MEMORY,ALUOut_MEMORY}), 
    						   .d({PCplus2_EXECUTE,DataOut_EXECUTE,WriteData_EXECUTE,ALUOut_EXECUTE}), 
    						   .clk(clk), .rst(rst));   		
-   	// memory-writeback data flip flop	
+   	// Memory/Writeback	
    	dff MW_DATA_FF [47:0] (.q({PCplus2_WB,MemOut_WB,DataOut_WB}), 
    						   .d({PCplus2_MEMORY,MemOut_MEMORY,DataOut_MEMORY}), 
    						   .clk(clk), .rst(rst));
-   						   
+
+
    	////////////////////////////
-   	// Instruction Pipepline //
+   	// INSTRUCTION PIPELINE  //
    	//////////////////////////
-   	// fetch-decode flip flop
+	/* Contains "latches" for instructions */
+
+   	// Fetch/Decode
    	dff FD_Inst [15:0] (.q(instr_DECODE), .d((rst | BrJmpTaken) ? (16'h0800) : (instr_IN)), .clk(clk), .rst(1'b0));
-   	// decode-execute flip flop
+   	// Decode/Execute
    	dff DX_Inst [15:0] (.q(instr_EXECUTE), .d((rst | BrJmpTaken) ? (16'h0800) : (instr_DECODE)), .clk(clk), .rst(1'b0));   	
-   	// execute-memory flip flop
+   	// Execute/Memory
    	dff XM_Inst [15:0] (.q(instr_MEMORY), .d((rst) ? (16'h0800) : (instr_EXECUTE)), .clk(clk), .rst(1'b0));   	
-   	// memory-writeback flip flop
+   	// Memory/Writeback
    	dff MW_Inst [15:0] (.q(instr_WB), .d((rst) ? (16'h0800) : (instr_MEMORY)), .clk(clk), .rst(1'b0));
    	
-	////////////////////////////
-	// Module Instantiations //
-	//////////////////////////
+
+	//////////////////////////////////
+	// STAGE MODULE INSTANTIATIONS //
+	////////////////////////////////
+	/* Instantiation of the 5 stages of the pipeline */
 	
-	// stall block module 
-	// checks for dependencies and inserts NOPs as needed
-	//  Produces a stall command as well
+	// Stall Block Module 
+	/* Checks for dependencies and inserts NOPs as needed
+	   Produces a stall command as well */
 	stallBlock STALL(.clk(clk), .rst(rst), .inst_If(instr_FETCH), .inst_IfId(instr_DECODE), .inst_IdEx(instr_EXECUTE), 
-	                 .inst_ExMem(instr_MEMORY), .inst_out(instr_IN), .stall(stall));
+	                 .inst_ExMem(instr_MEMORY), .inst_out(instr_IN), .stall(Stall));
    
-    // fetch module
-	fetch FETCH(.clk(clk), .rst(rst), .JBAdr(JBAdr), 	// fetch data inputs
-   		.PCVal(ControlSignals_EXECUTE[20]), .Enable(ControlSignals_DECODE[7]), .Dump(Dump_FETCH), .stall(stall), .BrJmpTaken(BrJmpTaken),	// fetch control inputs
-   		.instr(instr_FETCH), .PCplus2(PCplus2_FETCH));	// fetch outputs
+    // Fetch
+	fetch FETCH(.clk(clk), .rst(rst), .JBAdr(JBAdr), 																						// fetch data inputs
+   		.PCVal(ControlSignals_EXECUTE[20]), .Enable(ControlSignals_DECODE[7]), .Dump(Dump_FETCH), .stall(Stall), .BrJmpTaken(BrJmpTaken),	// fetch control inputs
+   		.instr(instr_FETCH), .PCplus2(PCplus2_FETCH)); 																						// fetch outputs
    	
-   	// decode module
-	decode DECODE(.clk(clk), .rst(rst), .instr(instr_DECODE), .instr_wb(instr_WB), .RegData(RegData_WB),					// decode data inputs
-   		 .RegDst(ControlSignals_WB[3:2]), .RegWrite(ControlSignals_WB[1]), 												// decode control inputs
-   		 .Reg1(Reg1_DECODE), .Reg2(Reg2_DECODE), 
-   		 .JumpOffset(JumpOffset_DECODE), .instr_imm(instr_imm_DECODE), .Control(Control));	// decode outputs
+   	// Decode
+	decode DECODE(.clk(clk), .rst(rst), .instr(instr_DECODE), .instr_wb(instr_WB), .RegData(RegData_WB),	// decode data inputs
+   		 .RegDst(ControlSignals_WB[3:2]), .RegWrite(ControlSignals_WB[1]), 									// decode control inputs
+   		 .Reg1(Reg1_DECODE), .Reg2(Reg2_DECODE), 															// ^^^^^^^^^^^^^^^^^^^^^
+   		 .JumpOffset(JumpOffset_DECODE), .instr_imm(instr_imm_DECODE), .Control(Control));					// decode outputs
    		    	
-   	// execute module
+   	// Execute
 	execute EXECUTE(.clk(clk), .rst(rst), .Reg1(Reg1_EXECUTE), .Reg2(Reg2_EXECUTE), .JumpOffset(JumpOffset_EXECUTE), 
-		.PCplus2(PCplus2_EXECUTE), .instr_imm(instr_imm_EXECUTE), 																	// execute data inputs
+		.PCplus2(PCplus2_EXECUTE), .instr_imm(instr_imm_EXECUTE), 																								// execute data inputs
    		.ExtMode(ControlSignals_EXECUTE[19]), .IType(ControlSignals_EXECUTE[18]), .Reg1Rev(ControlSignals_EXECUTE[17]), 
    		.Reg1Shift(ControlSignals_EXECUTE[16]), .Zero1(ControlSignals_EXECUTE[15]), .Zero2(ControlSignals_EXECUTE[14]), 
-   		.ALUSrc(ControlSignals_EXECUTE[13]), .BrOp(ControlSignals_EXECUTE[23:21]), .Branch(ControlSignals_EXECUTE[12]), .Jump(ControlSignals_EXECUTE[11]), 
+   		.ALUSrc(ControlSignals_EXECUTE[13]), .BrOp(ControlSignals_EXECUTE[23:21]), .Branch(ControlSignals_EXECUTE[12]), .JumpType(ControlSignals_EXECUTE[11]), 
    		.CompCarry(ControlSignals_EXECUTE[10]), .ALUComp(ControlSignals_EXECUTE[9]), .ALUOp(ControlSignals_EXECUTE[26:24]), 
-   		.sign(ControlSignals_EXECUTE[8]), .PcVal(ControlSignals_EXECUTE[20]),	// execute control inputs
-   		.ALUOut(ALUOut_EXECUTE), .DataOut(DataOut_EXECUTE), .JBAdr(JBAdr), .WriteData(WriteData_EXECUTE), .BrJmpTaken(BrJmpTaken)); 					// execute outputs (JBAdr ties directly to fetch)
+   		.sign(ControlSignals_EXECUTE[8]), .PCVal(ControlSignals_EXECUTE[20]),																					// execute control inputs
+   		.ALUOut(ALUOut_EXECUTE), .DataOut(DataOut_EXECUTE), .JBAdr(JBAdr), .WriteData(WriteData_EXECUTE), .BrJmpTaken(BrJmpTaken)); 							// execute outputs (JBAdr ties directly to fetch)
    		
-   	// memory module
-	memory MEMORY(.clk(clk), .rst(rst), .ALUOut(ALUOut_MEMORY), .WriteData(WriteData_MEMORY),	// memory data inputs
-   		.MemWrite(ControlSignals_MEMORY[5]), .Enable(ControlSignals_MEMORY[7]), .Dump(ControlSignals_MEMORY[6]), 										// memory control inputs
+   	// Memory
+	memory MEMORY(.clk(clk), .rst(rst), .ALUOut(ALUOut_MEMORY), .WriteData(WriteData_MEMORY),						// memory data inputs
+   		.MemWrite(ControlSignals_MEMORY[5]), .Enable(ControlSignals_MEMORY[7]), .Dump(ControlSignals_MEMORY[6]), 	// memory control inputs
    		.MemOut(MemOut_MEMORY)); // memory outputs	
    		
-   	// writeback module
+   	// Writeback
 	wb WRITEBACK(.clk(clk), .rst(rst), .DataOut(DataOut_WB), .MemOut(MemOut_WB), .PCplus2(PCplus2_WB), 	// wb data inputs
-   		.MemtoReg(ControlSignals_WB[4]), .AdrLink(ControlSignals_WB[0]), 														// wb control inputs
-   		.RegData(RegData_WB));																				// wb outputs
+   		.MemtoReg(ControlSignals_WB[4]), .AdrLink(ControlSignals_WB[0]), 								// wb control inputs
+   		.RegData(RegData_WB));																			// wb outputs
    		
    	assign Halt = ~(|instr_WB[15:11]);
    	
-   	dff WF_CTRL_FF (.q(Dump_FETCH), .d(Halt), .clk(clk), .rst(rst));
-    /*
-   	always @* case (fill)
-   		16'h0000: begin
-   			instr_DECODE = 16'h0800;
-   			instr_EXECUTE = 16'h0800;
-   			instr_MEMORY = 16'h0800;
-   			instr_WB = 16'h0800;
-   		end	
-   		16'h0001: begin
-   			instr_DECODE = instr_DECODE_temp;
-   			instr_EXECUTE = 16'h0800;
-   			instr_MEMORY = 16'h0800;
-   			instr_WB = 16'h0800;
-   		end	
-   		16'h0002: begin
-   			instr_DECODE = instr_DECODE_temp;
-   			instr_EXECUTE = instr_EXECUTE_temp;
-   			instr_MEMORY = 16'h0800;
-   			instr_WB = 16'h0800;
-   		end	
-   		16'h0003: begin
-   			instr_DECODE = instr_DECODE_temp;
-   			instr_EXECUTE = instr_EXECUTE_temp;
-   			instr_MEMORY = instr_MEMORY_temp;
-   			instr_WB = 16'h0800;
-   		end
-   		default: begin
-   			instr_DECODE = instr_DECODE_temp;
-   			instr_EXECUTE = instr_EXECUTE_temp;
-   			instr_MEMORY = instr_MEMORY_temp;
-   			instr_WB = instr_WB_temp;
-   		end
-   	endcase	
-   	*/			
+   	dff WF_CTRL_FF (.q(Dump_FETCH), .d(Halt), .clk(clk), .rst(rst));		
+
+
    				
 endmodule // proc
 // DUMMY LINE FOR REV CONTROL :0:
