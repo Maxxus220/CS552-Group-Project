b7109f0d2f96569089b9a86cc964ad6976656b31
Michael Feist <Maxxus220@gmail.com>
2022-11-27 16:30:20 -0600

    Cache + Direct and Direct Mem (Believe direct mem is not done)

--- cache/cache_direct/verilog/cache_controller.v
+++ cache/cache_direct/verilog/cache_controller.v
@@ -1,7 +1,7 @@
 module cache_controller(
-    rd, wr, hit, dirty, valid, busy, offset,                // Inputs
-    enable, comp, write, mem_wr, mem_rd, valid_in, done     // Outputs
-    clk, rst);                                              // Clk & Rst
+    rd, wr, hit, dirty, valid, busy, offset,                 // Inputs
+    enable, comp, write, mem_wr, mem_rd, valid_in, done      // Outputs
+    clk, rst);                                               // Clk & Rst
 
 ////////////
 // PORTS //
@@ -46,80 +46,80 @@ to set next_state
 
                 // WAIT
                 3'd0: begin
-                    assign enable = 1'b0;
-                    assign comp = 1'b0;
-                    assign write = 1'b0;
-                    assign mem_wr = 1'b0;
-                    assign mem_rd = 1'b0;
-                    assign valid_in = 1'b0;
-                    assign done = 1'b0;
+                    assign enable       = 1'b0;
+                    assign comp         = 1'b0;
+                    assign write        = 1'b0;
+                    assign mem_wr       = 1'b0;
+                    assign mem_rd       = 1'b0;
+                    assign valid_in     = 1'b0;
+                    assign done         = 1'b0;
 
                     assign next_state = ((rd | wr) ? (rd ? 1 : 3) : (0));
                 end
                 
                 // COMP_R
                 3'd1: begin
-                    assign enable = 1'b1;
-                    assign comp = 1'b1;
-                    assign write = 1'b0;
-                    assign mem_wr = 1'b0;
-                    assign mem_rd = 1'b0;
-                    assign valid_in = 1'b0;
-                    assign done = (hit & valid);
+                    assign enable       = 1'b1;
+                    assign comp         = 1'b1;
+                    assign write        = 1'b0;
+                    assign mem_wr       = 1'b0;
+                    assign mem_rd       = 1'b0;
+                    assign valid_in     = 1'b0;
+                    assign done         = (hit & valid);
 
                     assign next_state = ((hit & valid) ? 0 : 2);
                 end
 
                 // ACCESS_W
                 3'd2: begin
-                    assign enable = 1'b1;
-                    assign comp = 1'b0;
-                    assign write = 1'b1;
-                    assign mem_wr = 1'b0;
-                    assign mem_rd = 1'b1;
-                    assign valid_in = 1'b1;
-                    assign done = 1'b0;
+                    assign enable       = 1'b1;
+                    assign comp         = 1'b0;
+                    assign write        = 1'b1;
+                    assign mem_wr       = 1'b0;
+                    assign mem_rd       = 1'b1;
+                    assign valid_in     = 1'b1;
+                    assign done         = 1'b0;
 
                     assign next_state = (|busy ? 2 : 1);
                 end
 
                 // COMP_W
                 3'd3: begin
-                    assign enable = 1'b1;
-                    assign comp = 1'b1;
-                    assign write = 1'b1;
-                    assign mem_wr = 1'b0;
-                    assign mem_rd = 1'b0;
-                    assign valid_in = 1'b0;
-                    assign done = 1'b0;
+                    assign enable       = 1'b1;
+                    assign comp         = 1'b1;
+                    assign write        = 1'b1;
+                    assign mem_wr       = 1'b0;
+                    assign mem_rd       = 1'b0;
+                    assign valid_in     = 1'b0;
+                    assign done         = 1'b0;
 
                     assign next_state = ((hit & valid) ? 4 : 5);
                 end 
 
                 // CACHE_+_DIRECT
                 3'd4: begin
-                    assign enable = 1'b0;
-                    assign comp = 1'b0;
-                    assign write = 1'b0;
-                    assign mem_wr = 1'b0;
-                    assign mem_rd = 1'b0;
-                    assign valid_in = 1'b0;
-                    assign done = 1'b0;
+                    assign enable       = 1'b1;
+                    assign comp         = 1'b1;
+                    assign write        = 1'b1;
+                    assign mem_wr       = 1'b1;
+                    assign mem_rd       = 1'b0;
+                    assign valid_in     = 1'b0;
+                    assign done         = ~(|busy);
 
-
+                    assign next_state = (|busy ? 4 : 0);
                 end
 
                 // DIRECT_MEM
                 3'd5: begin
-                    assign enable = 1'b0;
-                    assign comp = 1'b0;
-                    assign write = 1'b0;
-                    assign mem_wr = 1'b0;
-                    assign mem_rd = 1'b0;
-                    assign valid_in = 1'b0;
-                    assign done = 1'b0;
+                    assign enable       = 1'b0;
+                    assign comp         = 1'b0;
+                    assign write        = 1'b0;
+                    assign mem_wr       = 1'b1;
+                    assign mem_rd       = 1'b0;
+                    assign valid_in     = 1'b0;
+                    assign done         = ~(|busy);
 
-
+                    assign next_state = (|busy ? 5 : 0);
                 end
 
                 default: 
