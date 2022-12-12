4c12519add981ee0d12fa4c4c3ff9c4f0f40a50e
SmithyLK <50187468+SmithyLK@users.noreply.github.com>
2022-12-12 17:41:20 -0600

    added dff with enable

---
+++ dff_en.v
@@ -0,0 +1,15 @@
+// D-flipflop wrapper with enable
+
+module dff_en (q, d, en, clk, rst);
+
+    output         q;
+    input          d;
+	input		   en;
+    input          clk;
+    input          rst;
+
+	wire temp
+	assign temp = (en ? d : q);
+    dff FF(.q(q), .d(temp), .clk(clk), .rst(rst));
+
+endmodule
